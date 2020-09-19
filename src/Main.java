2a38a8d81b8dc3bfbca4ed3f3469e0d2a6e3648a
TPD <mig.mic@gmail.com>
2020-09-19 15:10:24 +0200

    ex 5 +6

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,50 @@
+import java.util.ArrayList;
+
 public class Main {
+    Orbital earth = new Planet("Planet", "Earth", "Sun", "5.972E24", 6371, 149600000);
+    Orbital moon = new Moon("Moon", "Moon", earth.getName(), "7.348E22", 1737, 384400);
+    Orbital mars = new Planet("Planet","Mars", "Sun", "6.39E23", 3390, 227940000);
+    Orbital phobos = new Moon("Moon", "Phobos", "Mars", "10.60E15", 11, 6800);
+    Orbital deimos = new Moon("Moon", "Deimos", "Mars", "1.48E15", 6, 20069);
     public static void main(String[] args) {
-        Orbital earth = new Planet("Planet", "Earth", "Sun", "5.972E24", 6371, 149600000);
-        System.out.println(earth);
-        Orbital moon = new Moon("Moon", "Moon", earth.getName(), "7.348E22", 1737, 384400);
-        System.out.println(moon);
+
+        //System.out.println(earth);
+
+        //System.out.println(moon);
     }
 
     public void moonsofplanets(){
 
+
     }
+
+    public int moonsForPlanet(Orbital e) {
+        int count = 0;
+        ArrayList<Orbital> orbList = new ArrayList<>();
+        orbList.add(earth);
+        orbList.add(moon);
+        for (Orbital s : orbList) {
+            if (s.getType().equals("Moon") && s.getOrbit().equals(e.getName())) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        return count;
+
+    }
+
+    public int distanceBetweenPlanets(Orbital e, Orbital c) {
+        int dist = 0;
+        if (e.getDistanceToOrbitKM() > c.getDistanceToOrbitKM()) {
+            dist = e.getDistanceToOrbitKM() - c.getDistanceToOrbitKM();
+
+        } else {
+            dist = c.getDistanceToOrbitKM() - e.getDistanceToOrbitKM();
+        }
+
+        System.out.println(dist);
+        return dist;
+    }
+
+
 }
--- test/MainTest.java
+++ test/MainTest.java
@@ -1,10 +1,19 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
     Orbital e = new Planet("Planet", "Earth", "Sun", "5.972E24", 6371, 149600000);
-    Orbital m = new Moon("Moon", "Moon", e.getName(), "7.348E22", 1737, 384400);
+    Orbital m = new Moon("Moon", "Moon", "Earth", "7.348E22", 1737, 384400);
+    Orbital mars = new Planet("Planet","Mars", "Sun", "6.39E23", 3390, 227940000);
+    Orbital phobos = new Moon("Moon", "Phobos", "Mars", "10.60E15", 11, 6800);
+    Orbital deimos = new Moon("Moon", "Deimos", "Mars", "1.48E15", 6, 20069);
+    ArrayList<Orbital> orbList = new ArrayList<>();
+    Main test = new Main();
+
 
     @Test
     void testEarthObject() {
@@ -19,7 +28,6 @@ class MainTest {
 
     @Test
     void testMoonObject(){
-
         assertEquals("Moon", m.getType());
         assertEquals("Moon", m.getName());
         assertEquals(m.getOrbit(), e.getName());
@@ -29,7 +37,23 @@ class MainTest {
     }
 
     @Test
-    void moonsofplanets() {
-        if(m.orbit.contains(e.getName()))
+    void testPlant(){
+
+        orbList.add(e);
+        orbList.add(m);
+        assertEquals(1,test.moonsForPlanet(e));
     }
+
+    @Test
+    void distPlanets(){
+        orbList.add(e);
+        orbList.add(mars);
+       // assertEquals(149215600, test.distanceBetweenPlanets(e,mars));
+        assertEquals(mars.getDistanceToOrbitKM()-e.getDistanceToOrbitKM(), test.distanceBetweenPlanets(e,mars));
+
+
+    }
+
+
+
 }
